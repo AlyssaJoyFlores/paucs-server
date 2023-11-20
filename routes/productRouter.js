f92846344e70dab61010b3b6fd44fd5998fa424a
AlyssaJoyFlores <114978911+AlyssaJoyFlores@users.noreply.github.com>
2023-11-20 21:55:53 +0800

    Add getSingleProduct Router

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 const {
     getAllProducts,
+    getSingleProduct,
     addProduct,
     updateProduct,
     deleteProduct,
@@ -15,6 +16,7 @@ const {
 router.route('/getProducts').get(getAllProducts);
 router.route('/uploadProdImage').post(uploadProdImage);
 router.route('/addProduct').post(addProduct);
+router.route('/getSingleProduct/:id').get(getSingleProduct);
 router.route('/updateProdImage/:id').post(updateProdImage);
 router.route('/updateProduct/:id').patch(updateProduct);
 router.route('/deleteProduct/:id').delete(deleteProduct);
